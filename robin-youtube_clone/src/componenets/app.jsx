448745ea84e335a94676f14a60e20993a172c94b
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-04-26 12:32:08 -0600

    Adding in src files, components

---
+++ robin-youtube_clone/src/componenets/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TitleBar from './TitleBar/titleBar';
+
+class App extends Component {
+    state = {
+        return() {
+            return (
+                <div className='container-fluid'>
+                    <TitleBar />
+                </div>
+            );
+        }
+    }
+}
+
+export default App;
