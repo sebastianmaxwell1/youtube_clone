6ce278c880f191bcec1d0ba28ec1045672ecc74e
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-04-28 14:35:48 -0400

    created currentvideo.js with embedded video. searchbar inop

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,12 @@
 //libraries
-import React, { Component } from 'react';
-import YTSearch from 'youtube-api-search'
-import './app.css';
-import VideoList from './VideoList'
+import React from 'react';
+// import YTSearch from 'youtube-api-search'
+// import VideoList from './VideoList'
+// import "video.css";
+import YoutubeEmbed from "./CurrentVideo";
 
 //components
-import Searchbar from './SearchBar'
+// import Searchbar from './SearchBar'
 
 // function App() {
 //     return (
@@ -15,32 +16,46 @@ import Searchbar from './SearchBar'
 //     );
 // }
 
-class Video extends Component {
-    state = {
-        videos: [],
-        currentVideo: null
-    }
+// class Video extends Component {
+//     state = {
+//         videos: [],
+//         currentVideo: null
+//     }
     
     // key:'API_KEY' once I have a database
-    handleSubmit = (searchBarSearch) => {
-        YTSearch({
-            key: '', 
-            term: searchBarSearch},
-            (videos) => {
-                this.setState({
-                    videos: videos,
-                    currentVideo: videos[0]
-                })
-            })
-        }
+    // handleSubmit = (searchBarSearch) => {
+    //     YTSearch({
+    //         key: 'AIzaSyAWKp-G5XJ6SJw1rOgvQVMPo4x4ZVsKip8', 
+    //         term: searchBarSearch},
+    //         (videos) => {
+    //             this.setState({
+    //                 videos: videos,
+    //                 currentVideo: videos[0]
+    //             })
+    //         })
+    //     }
 
-    render(){
-        return (
-            <div className="App">
-                <Searchbar handleSearchSubmit={this.handleSubmit}/>
-                <VideoList videos={this.state.videos} />
-            </div>
-        );
-        }
+
+//     render(){
+//         return (
+//             <div className="App">
+//                 <Searchbar handleSearchSubmit={this.handleSubmit}/>
+//                 <VideoList videos={this.state.videos} />
+//             </div>
+//         );
+//         }
+// }
+// export default Video;
+
+// import React from "react";
+// import "./styles.css";
+// import YoutubeEmbed from "./YoutubeEmbed";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Youtube Embed</h1>
+      <YoutubeEmbed embedId="opXobSMUvi4" />
+    </div>
+  );
 }
-export default Video;
